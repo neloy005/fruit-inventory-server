af700007384c025d073ddfa4bc065163e0076712
neloy005 <arifistiekofficial@gmail.com>
2022-05-03 23:14:06 +0600

    delete api created

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const fruit = await fruitCollection.findOne(query);
             res.send(fruit);
-        })
+        });
 
 
         app.put('/fruit/:id', async (req, res) => {
@@ -44,6 +44,13 @@ async function run() {
                 }
             };
             const result = await fruitCollection.updateOne(filter, updatedDoc, options)
+            res.send(result);
+        });
+
+        app.delete('/fruit/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await fruitCollection.deleteOne(query);
             res.send(result);
         })
     }
